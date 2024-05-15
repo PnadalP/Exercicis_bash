91572a9f3d3483bfa1e471b95669fed249104d2c
Your Name <you@example.com>
2024-05-15 12:00:33 +0000

    beta del joc fred i calent

---
+++ fred_i_calent.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+#Genera un nombre aleatori entre 1 i 10:
+adivina=$(( ( RANDOM % 10 )  + 1 ))
+########################################################
+# Escriu el teu codi aqui
+########################################################
+i=1
+for i in {1..3} ;do
+read -p "Tens 3 intents per endivinar el nombre del 1 al 10: " x
+if [ $x -eq $adivina ]
+then echo "eureka!!"
+break
+else
+echo "has usat el intent ${i}"
+fi
+done
+if [[ $x -ne $adivina ]]
+then
+########################################################
+# Fi del teu codi
+########################################################
+
+echo "El nombre que havies d'adivinar era: ${adivina}"
+fi
