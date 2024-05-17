b69ae152d90ba2eee629654af34a5c9c6e45d164
Your Name <you@example.com>
2024-05-17 06:28:11 +0000

    Versió corregida fred i calent

--- fred_i_calent.sh
+++ fred_i_calent.sh
@@ -7,7 +7,8 @@ adivina=$(( ( RANDOM % 10 )  + 1 ))
 ########################################################
 i=1
 for i in {1..3} ;do
-read -p "Tens 3 intents per endivinar el nombre del 1 al 10: " x
+let intents=4-i
+read -p "Tens ${intents} intents per endivinar el nombre del 1 al 10: " x
 if [ $x -eq $adivina ]
 then echo "eureka!!"
 break
