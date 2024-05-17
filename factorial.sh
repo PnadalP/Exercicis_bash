6352ae3dffce19f324ec9106701f955c2ab702cb
Your Name <you@example.com>
2024-05-17 07:47:30 +0000

    factorial arreglat

--- factorial.sh
+++ factorial.sh
@@ -4,7 +4,7 @@ i=1
 factorial=1
 while [[ i -le nombre ]]
 do
-    factorial=$((factorial*i))
+    factorial=$(echo "$factorial*$i" | bc)
     i=$((i+1))
 done
 echo "el factorial de $nombre es: $factorial"
